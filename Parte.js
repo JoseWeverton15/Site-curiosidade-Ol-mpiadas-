64c2357c96b9dcb18fcaa1466a08ef422b881b18
Jose Weverton <joseweverton2003@outlook.com>
2024-03-28 23:30:48 -0300

    teste12

--- Parte.js
+++ Parte.js
@@ -22,35 +22,19 @@ const lerCSV = (caminhoArquivo, callback) => {
         });
 };
 
-const a = () => {
+// Função principal para processar o CSV e exibir o resultado na página HTML
+const exibirTotalMedalhasOuro = () => {
     lerCSV('atletas.csv', (error, dados) => {
         if (error) {
             console.error('Erro ao ler o arquivo CSV:', error);
         } else {
             const dadosFiltrados = filtrar(dados, 'Gold');
-            soma = somatorio(dadosFiltrados);
-            document.getElementById("resultado").innerText = "Total de medalhas de ouro: " + soma; // Atualiza o conteúdo do elemento com o resultado
+            const totalMedalhasOuro = somatorio(dadosFiltrados);
+            console.log("Total de medalhas de ouro:", totalMedalhasOuro); // Verifica se o total de medalhas está correto
+            document.getElementById('resultado').textContent = "Total de medalhas de ouro: " + totalMedalhasOuro;
         }
     });
 };
 
-a(); // Chama a função a ao carregar a página
-document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
-
-function handleFileSelect(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function(event) {
-        const csvData = event.target.result;
-        processData(csvData);
-    };
-
-    reader.readAsText(file);
-}
-
-function processData(csvData) {
-    const lines = csvData.split('\n');
-    const goldMedals = lines.filter(line => line.includes('Gold')).length;
-    document.getElementById('resultado').innerText = "Total de medalhas de ouro: " + goldMedals;
-}
+// Chamando a função principal para iniciar o processo
+exibirTotalMedalhasOuro();
